996a0d6c694340bd9dbd84484239456e90d7ed37
christinah <christinewasabi@gmail.com>
2019-12-27 23:49:15 +0300

    add rainbow-hat driver

---
+++ app/src/main/java/xyz/megundo/rainbowhello/MainActivity.kt
@@ -0,0 +1,18 @@
+package xyz.megundo.rainbowhello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+private val TAG = MainActivity::class.java.simpleName
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
