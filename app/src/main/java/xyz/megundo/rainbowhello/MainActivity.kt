59eef8293d69377e003384930acb740778b60f13
christinah <christinewasabi@gmail.com>
2019-12-28 21:18:50 +0300

    simple rainbow-hat

--- app/src/main/java/xyz/megundo/rainbowhello/MainActivity.kt
+++ app/src/main/java/xyz/megundo/rainbowhello/MainActivity.kt
@@ -2,17 +2,39 @@ package xyz.megundo.rainbowhello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat
+import com.google.android.things.pio.Gpio
 
 private val TAG = MainActivity::class.java.simpleName
 
 class MainActivity : AppCompatActivity() {
+    var redLED:Gpio?=null
+    val blinkHandler=Handler()
+
+    val blinkRunnable=object:Runnable{
+        override fun run() {
+
+            redLED?.also {gpio->
+                gpio.value=!gpio.value
+
+                blinkHandler.postDelayed(this,1000)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        redLED=RainbowHat.openLedRed()
+
+        blinkHandler.postDelayed(blinkRunnable,1000)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        blinkHandler.removeCallbacks(blinkRunnable)
+        redLED?.close()
     }
 
 }
